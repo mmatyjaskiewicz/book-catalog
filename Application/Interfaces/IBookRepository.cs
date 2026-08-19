41f17b7c7e257a40ad92d2f67e131d6d88ce2a2d
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-19 13:49:42 +0200

    feat: implement book application service and repository interface

---
+++ Application/Interfaces/IBookRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces;
+
+public interface IBookRepository
+{
+    Task AddAsync(Book book);
+    Task<List<Book>> GetAllAsync();
+    Task<Book?> GetByIdAsync(Guid id);
+    Task DeleteAsync(Guid id);
+}
---
+++ Application/Services/BookService.cs
@@ -0,0 +1,30 @@
+﻿using Application.Interfaces;
+using Domain.Entities;
+
+namespace Application.Services;
+
+public class BookService(IBookRepository bookRepository)
+{
+    public async Task<Book> CreateAsync(string title, string author, int year)
+    {
+        var book = new Book(title, author, year);
+
+        await bookRepository.AddAsync(book);
+        return book;
+    }
+    
+    public async Task<List<Book>> GetAllAsync()
+    {
+        return await bookRepository.GetAllAsync();
+    }
+    
+    public async Task<Book?> GetByIdAsync(Guid id)
+    {
+        return await bookRepository.GetByIdAsync(id);
+    }
+    
+    public async Task DeleteAsync(Guid id)
+    {
+        await bookRepository.DeleteAsync(id);
+    }
+}
