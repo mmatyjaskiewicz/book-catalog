7889ff7538c394ce8b1ecb65fbafc4360afc7682
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-20 14:28:21 +0200

    feat: add global exception handler

---
+++ WebApi/Exceptions/GlobalExceptionHandler.cs
@@ -0,0 +1,43 @@
+﻿using Application.Exceptions;
+using Application.Exceptions.BadRequest;
+using Application.Exceptions.NotFound;
+using Microsoft.AspNetCore.Diagnostics;
+
+namespace WebApi.Exceptions;
+
+public class GlobalExceptionHandler : IExceptionHandler
+{
+    public async ValueTask<bool> TryHandleAsync(HttpContext httpContext, Exception exception, CancellationToken cancellationToken)
+    {
+        var title = exception switch
+        {
+            BadRequestException => "Bad Request",
+            NotFoundException => "Not Found",
+            _ => "Internal Server Error"
+        };
+        
+        var statusCode = exception switch
+        {
+            BadRequestException => StatusCodes.Status400BadRequest,
+            NotFoundException => StatusCodes.Status404NotFound,
+            _ => StatusCodes.Status500InternalServerError
+        };
+        
+        var message = exception switch
+        {
+            AppException => exception.Message,
+            _ => "Internal server error"
+        };
+
+        httpContext.Response.StatusCode = statusCode;
+
+        await httpContext.Response.WriteAsJsonAsync(new
+        {
+            Title = title,
+            Status = statusCode,
+            error = message
+        }, cancellationToken);
+
+        return true;
+    }
+}
