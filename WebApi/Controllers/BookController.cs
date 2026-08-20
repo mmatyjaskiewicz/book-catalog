67760bbad45fd47f67a2aaeec79e4a9294b59c95
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-20 13:08:07 +0200

    feat: add book controller

---
+++ WebApi/Controllers/BookController.cs
@@ -0,0 +1,47 @@
+﻿using Application.DTOs.Requests;
+using Application.Interfaces;
+using Application.Services;
+using Domain.Entities;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApi.Controllers;
+
+[ApiController]
+[Route("api/[controller]")]
+public class BookController(BookService bookService) : ControllerBase
+{
+    [HttpGet]
+    public async Task<IActionResult> GetAll()
+    {
+        var books = await bookService.GetAllAsync();
+        return Ok(books);
+    }
+
+    [HttpGet("{id}")]
+    public async Task<IActionResult> GetById(Guid id)
+    {
+        var book = await bookService.GetByIdAsync(id);
+        return Ok(book);
+    }
+
+    [HttpPost]
+    public async Task<IActionResult> Create(CreateBookRequest request)
+    {
+        await bookService.CreateAsync(request);
+        return Created();
+    }
+
+    [HttpDelete("{id}")]
+    public async Task<IActionResult> Delete(Guid id)
+    {
+        await bookService.DeleteAsync(id);
+        return NoContent();
+    }
+
+    [HttpPatch("{id}")]
+    public async Task<IActionResult> Update(Guid id, UpdateBookRequest request)
+    {
+        var updatedBook = await bookService.UpdateAsync(id, request);
+        return Ok(updatedBook);
+    }
+}
