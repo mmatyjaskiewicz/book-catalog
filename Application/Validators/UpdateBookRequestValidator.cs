512953a5f8ce0ec771d26f7ae1825e0f766f7a6a
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-20 18:15:01 +0200

    feat: add validation filter and update validation rules

--- Application/Validators/UpdateBookRequestValidator.cs
+++ Application/Validators/UpdateBookRequestValidator.cs
@@ -8,15 +8,15 @@ public class UpdateBookRequestValidator : AbstractValidator<UpdateBookRequest>
     public UpdateBookRequestValidator()
     {
         RuleFor(x => x.Title)
-            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.")
-            .When(x => !string.IsNullOrEmpty(x.Title));
-
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+        
         RuleFor(x => x.Author)
+            .NotEmpty().WithMessage("Author is required.")
             .MaximumLength(100).WithMessage("Author must not exceed 100 characters.")
-            .When(x => !string.IsNullOrEmpty(x.Author));
 
         RuleFor(x => x.Year)
+            .NotEmpty().WithMessage("Year is required.")
             .InclusiveBetween(1, DateTime.Now.Year).WithMessage($"Year must be between 1 and {DateTime.Now.Year}.")
-            .When(x => x.Year.HasValue);
     }
 }
