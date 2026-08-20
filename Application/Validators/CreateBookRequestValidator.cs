4e781c1f5df9e00d35009b17f33f128d0621f5a2
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-20 17:47:48 +0200

    feat: add request input validators

---
+++ Application/Validators/CreateBookRequestValidator.cs
@@ -0,0 +1,21 @@
+﻿using Application.DTOs.Requests;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class CreateBookRequestValidator : AbstractValidator<CreateBookRequest>
+{
+    public CreateBookRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .NotEmpty().WithMessage("Title is required.")
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
+
+        RuleFor(x => x.Author)
+            .NotEmpty().WithMessage("Author is required.")
+            .MaximumLength(100).WithMessage("Author must not exceed 100 characters.");
+        
+        RuleFor(x => x.Year).
+            InclusiveBetween(1, DateTime.Now.Year).WithMessage($"Year must be between 1 and {DateTime.Now.Year}.");
+    }
+}
---
+++ Application/Validators/UpdateBookRequestValidator.cs
@@ -0,0 +1,22 @@
+﻿using Application.DTOs.Requests;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class UpdateBookRequestValidator : AbstractValidator<UpdateBookRequest>
+{
+    public UpdateBookRequestValidator()
+    {
+        RuleFor(x => x.Title)
+            .MaximumLength(100).WithMessage("Title must not exceed 100 characters.")
+            .When(x => !string.IsNullOrEmpty(x.Title));
+
+        RuleFor(x => x.Author)
+            .MaximumLength(100).WithMessage("Author must not exceed 100 characters.")
+            .When(x => !string.IsNullOrEmpty(x.Author));
+
+        RuleFor(x => x.Year)
+            .InclusiveBetween(1, DateTime.Now.Year).WithMessage($"Year must be between 1 and {DateTime.Now.Year}.")
+            .When(x => x.Year.HasValue);
+    }
+}
