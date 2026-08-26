f3456c0936a4ba0eb3f65d5084e53e3c785cea8e
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-26 17:33:08 +0200

    test: initialize validators tests in the project

---
+++ UnitTests/Validators/CreateBookRequestValidatorTests.cs
@@ -0,0 +1,48 @@
+﻿using Application.DTOs.Requests;
+using Application.Validators;
+using FluentValidation.TestHelper;
+
+namespace UnitTests.Validators;
+
+public class CreateBookRequestValidatorTests
+{
+    [Fact]
+    public void ShouldBeValid_WhenRequestIsCorrect()
+    {
+        // Arrange
+        var validator = new CreateBookRequestValidator();
+
+        var request = new CreateBookRequest
+        {
+            Title = "Mock title",
+            Author = "Mock author",
+            Year = 2024
+        };
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldNotHaveAnyValidationErrors();
+    }
+    
+    [Fact]
+    public void ShouldHaveError_WhenTitleIsEmpty()
+    {
+        // Arrange
+        var validator = new CreateBookRequestValidator();
+
+        var request = new CreateBookRequest
+        {
+            Title = "",
+            Author = "Mock author",
+            Year = 2024
+        };
+
+        // Act
+        var result = validator.TestValidate(request);
+
+        // Assert
+        result.ShouldHaveValidationErrorFor(x => x.Title);
+    }
+}
