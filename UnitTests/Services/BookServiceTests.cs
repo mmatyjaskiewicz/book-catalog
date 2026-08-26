835d3ee2dae44eaed4d88847a87d786fbd71a2cb
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-26 15:50:50 +0200

    test: add tests for GetByIdAsync method

---
+++ UnitTests/Services/BookServiceTests.cs
@@ -0,0 +1,52 @@
+﻿using Application.Exceptions.NotFound;
+using Application.Interfaces;
+using Application.Services;
+using Domain.Entities;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace UnitTests.Services;
+
+public class BookServiceTests
+{
+    [Fact]
+    public async Task GetByIdAsync_ShouldReturnBook_WhenBookExists()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IBookRepository>();
+        var loggerMock = new Mock<ILogger<BookService>>();
+        
+        var book = new Book("Mock title", "Mock author", 2024);
+        
+        repositoryMock
+            .Setup(x => x.GetByIdAsync(book.Id))
+            .ReturnsAsync(book);
+        
+        var bookService = new BookService(repositoryMock.Object, loggerMock.Object);
+        
+        // Act
+        var result = await bookService.GetByIdAsync(book.Id);
+        
+        // Assert
+        Assert.Equal(book, result);
+    }
+
+    [Fact]
+    public async Task GetByIdAsync_ShouldThrowNotFoundException_WhenBookDoesNotExist()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IBookRepository>();
+        var loggerMock = new Mock<ILogger<BookService>>();
+        
+        var fakeBookId = Guid.NewGuid();
+        
+        repositoryMock
+            .Setup(x => x.GetByIdAsync(fakeBookId))
+            .ReturnsAsync((Book)null!);
+        
+        var bookService = new BookService(repositoryMock.Object, loggerMock.Object);
+        
+        // Act & Assert
+        await Assert.ThrowsAsync<NotFoundException>(() => bookService.GetByIdAsync(fakeBookId));
+    }
+}
