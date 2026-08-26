ef955bf7565df82612f2bd0cfd8490880f9ff647
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-26 16:04:36 +0200

    test: add CreateAsync and DeleteAsync tests

--- UnitTests/Services/BookServiceTests.cs
+++ UnitTests/Services/BookServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using Application.Exceptions.NotFound;
+﻿using Application.DTOs.Requests;
+using Application.Exceptions.NotFound;
 using Application.Interfaces;
 using Application.Services;
 using Domain.Entities;
@@ -9,6 +10,10 @@ namespace UnitTests.Services;
 
 public class BookServiceTests
 {
+    // Tests for GetAllAsync method in BookService
+    // TODO: Add GetAllAsync tests
+    
+    // Tests for GetByIdAsync method in BookService
     [Fact]
     public async Task GetByIdAsync_ShouldReturnBook_WhenBookExists()
     {
@@ -49,4 +54,77 @@ public class BookServiceTests
         // Act & Assert
         await Assert.ThrowsAsync<NotFoundException>(() => bookService.GetByIdAsync(fakeBookId));
     }
+    
+    // Tests for CreateAsync method in BookService
+    [Fact]
+    public async Task CreateAsync_ShouldCallRepositoryCreate_WhenCalled()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IBookRepository>();
+        var loggerMock = new Mock<ILogger<BookService>>();
+        
+        var bookService = new BookService(repositoryMock.Object, loggerMock.Object);
+        
+        var request = new CreateBookRequest
+        {
+            Title = "Mock title",
+            Author = "Mock author",
+            Year = 2024
+        };
+        
+        await bookService.CreateAsync(request);
+        
+        repositoryMock.Verify(
+            x => x.AddAsync(It.Is<Book>(book =>
+                book.Title == request.Title &&
+                book.Author == request.Author &&
+                book.Year == request.Year)),
+            Times.Once);
+    }
+    
+    // Tests for DeleteAsync method in BookService
+    [Fact]
+    public async Task DeleteAsync_ShouldDeleteBook_WhenBookExists()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IBookRepository>();
+        var loggerMock = new Mock<ILogger<BookService>>();
+
+        var book = new Book("Mock title", "Mock author", 2024);
+
+        repositoryMock
+            .Setup(x => x.GetByIdAsync(book.Id))
+            .ReturnsAsync(book);
+
+        var bookService = new BookService(repositoryMock.Object, loggerMock.Object);
+
+        // Act
+        await bookService.DeleteAsync(book.Id);
+
+        // Assert
+        repositoryMock.Verify(
+            x => x.DeleteAsync(book),
+            Times.Once);
+    }
+    
+    [Fact]
+    public async Task DeleteAsync_ShouldThrowNotFoundException_WhenBookDoesNotExist()
+    {
+        // Arrange
+        var repositoryMock = new Mock<IBookRepository>();
+        var loggerMock = new Mock<ILogger<BookService>>();
+
+        var bookId = Guid.NewGuid();
+
+        repositoryMock
+            .Setup(x => x.GetByIdAsync(bookId))
+            .ReturnsAsync((Book)null!);
+
+        var bookService = new BookService(repositoryMock.Object, loggerMock.Object);
+
+        // Act & Assert
+        await Assert.ThrowsAsync<NotFoundException>(() => bookService.DeleteAsync(bookId));
+    }
+    
+    // Tests for UpdateAsync method in BookService
 }
