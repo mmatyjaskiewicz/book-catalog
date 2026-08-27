29357b35f6d1be16d7120798ad955f6ff5b6b0f8
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-08-27 14:04:13 +0200

    fix: correct PATCH request validation

--- Application/Validators/UpdateBookRequestValidator.cs
+++ Application/Validators/UpdateBookRequestValidator.cs
@@ -8,15 +8,15 @@ public class UpdateBookRequestValidator : AbstractValidator<UpdateBookRequest>
     public UpdateBookRequestValidator()
     {
         RuleFor(x => x.Title)
-            .NotEmpty().WithMessage("Title is required.")
+            .NotEmpty().WithMessage("Title is required.").When(x => x.Title != null)
             .MaximumLength(100).WithMessage("Title must not exceed 100 characters.");
 
         RuleFor(x => x.Author)
-            .NotEmpty().WithMessage("Author is required.")
+            .NotEmpty().WithMessage("Author is required.").When(x => x.Author != null)
             .MaximumLength(100).WithMessage("Author must not exceed 100 characters.");
 
         RuleFor(x => x.Year)
-            .NotEmpty().WithMessage("Year is required.")
+            .NotEmpty().WithMessage("Year is required.").When(x => x.Year != null)
             .InclusiveBetween(1, DateTime.Now.Year).WithMessage($"Year must be between 1 and {DateTime.Now.Year}.");
     }
 }
