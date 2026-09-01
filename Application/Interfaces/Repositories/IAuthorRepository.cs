f08ff1483e3e2886b37c4247499855a0bebfcf3a
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-01 16:19:49 +0200

    feat: add author repository

---
+++ Application/Interfaces/Repositories/IAuthorRepository.cs
@@ -0,0 +1,10 @@
+﻿using Application.DTOs.Queries;
+using Application.Models;
+using Domain.Entities;
+
+namespace Application.Interfaces.Repositories;
+
+public interface IAuthorRepository : IRepository<Author>
+{
+    public Task<PagedResult<Author>> GetAllAsync(AuthorQueryParameters queryParameters);
+}
