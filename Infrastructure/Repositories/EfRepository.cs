48d9cf70b3d398a73c6ff9a71bb51e81e0350554
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-01 15:50:39 +0200

    refactor: create ef generic repository

---
+++ Infrastructure/Repositories/EfRepository.cs
@@ -0,0 +1,36 @@
+﻿using Application.Interfaces;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories;
+
+public class EfRepository<T>(BookCatalogDbContext context) : IRepository<T> where T : class
+{
+    public Task AddAsync(T entity)
+    {
+        context.Set<T>().Add(entity);
+        return context.SaveChangesAsync();
+    }
+
+    public Task UpdateAsync(T entity)
+    {
+        context.Set<T>().Update(entity);
+        return context.SaveChangesAsync();
+    }
+
+    public Task DeleteAsync(T entity)
+    {
+        context.Set<T>().Remove(entity);
+        return context.SaveChangesAsync();
+    }
+
+    public Task<List<T>> GetAllAsync()
+    {
+        return context.Set<T>().ToListAsync();
+    }
+
+    public Task<T?> GetByIdAsync(Guid id)
+    {
+        return context.Set<T>().FindAsync(id).AsTask();
+    }
+}
