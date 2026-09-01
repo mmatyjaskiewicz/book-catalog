d62aa418d55f3a36a871168fa4c3d95ab5bc730f
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-01 11:14:40 +0200

    feat: configure domain entity relationships

---
+++ Infrastructure/Persistence/Configurations/BookConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class BookConfiguration : IEntityTypeConfiguration<Book>
+{
+    public void Configure(EntityTypeBuilder<Book> builder)
+    {
+        builder.ToTable("books");
+
+        builder.HasKey(b => b.Id);
+
+        builder.Property(b => b.Id)
+            .HasColumnName("id");
+
+        builder.Property(b => b.Title)
+            .HasColumnName("title")
+            .IsRequired()
+            .HasMaxLength(200);
+
+        builder.Property(b => b.PublishYear)
+            .HasColumnName("publish_year")
+            .IsRequired();
+
+        builder.Property(b => b.AuthorId)
+            .HasColumnName("author_id")
+            .IsRequired();
+
+        builder.HasOne(b => b.Author)
+            .WithMany(a => a.Books)
+            .HasForeignKey(b => b.AuthorId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
---
+++ Infrastructure/Persistence/Configurations/LoanConfiguration.cs
@@ -0,0 +1,43 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Infrastructure.Persistence.Configurations;
+
+public class LoanConfiguration : IEntityTypeConfiguration<Loan>
+{
+    public void Configure(EntityTypeBuilder<Loan> builder)
+    {
+        builder.ToTable("loans");
+
+        builder.HasKey(l => l.Id);
+
+        builder.Property(l => l.Id)
+            .HasColumnName("id");
+
+        builder.Property(l => l.BookId)
+            .HasColumnName("book_id")
+            .IsRequired();
+
+        builder.Property(l => l.UserId)
+            .HasColumnName("user_id")
+            .IsRequired();
+
+        builder.Property(l => l.BorrowedAt)
+            .HasColumnName("borrowed_at")
+            .IsRequired();
+
+        builder.Property(l => l.ReturnedAt)
+            .HasColumnName("returned_at");
+
+        builder.HasOne(l => l.Book)
+            .WithMany()
+            .HasForeignKey(l => l.BookId)
+            .OnDelete(DeleteBehavior.Restrict);
+
+        builder.HasOne(l => l.User)
+            .WithMany(u => u.Loans)
+            .HasForeignKey(l => l.UserId)
+            .OnDelete(DeleteBehavior.Restrict);
+    }
+}
