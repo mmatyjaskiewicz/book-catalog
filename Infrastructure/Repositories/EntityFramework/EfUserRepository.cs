8fce4374f9b3807f36bb9f2fb47ca1c77ca7242d
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-02 15:51:29 +0200

    feat: add user repository foundation

---
+++ Infrastructure/Repositories/EntityFramework/EfUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence;
+
+namespace Infrastructure.Repositories.EntityFramework;
+
+public class EfUserRepository : EfRepository<User>, IUserRepository
+{
+    private readonly BookCatalogDbContext _context;
+    
+    public EfUserRepository(BookCatalogDbContext context) : base(context)
+    {
+        _context = context;
+    }
+}
