c2f82a9895cb7c288a37f730c2309000fd1056c4
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-02 15:44:12 +0200

    feat: add loan repository foundation

---
+++ Application/Interfaces/Repositories/ILoanRepository.cs
@@ -0,0 +1,8 @@
+﻿using Domain.Entities;
+
+namespace Application.Interfaces.Repositories;
+
+public interface ILoanRepository : IRepository<Loan>
+{
+    Task<Loan?> GetActiveLoanByBookIdAsync(Guid bookId);
+}
---
+++ Infrastructure/Repositories/EntityFramework/EfLoanRepository.cs
@@ -0,0 +1,21 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+using Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories.EntityFramework;
+
+public class EfLoanRepository : EfRepository<Loan>, ILoanRepository
+{
+    private readonly BookCatalogDbContext _context;
+
+    public EfLoanRepository(BookCatalogDbContext context) : base(context)
+    {
+        _context = context;
+    }
+
+    public async Task<Loan?> GetActiveLoanByBookIdAsync(Guid bookId)
+    {
+        return await _context.Loans.FirstOrDefaultAsync(l => l.BookId == bookId && l.ReturnedAt == null);
+    }
+}
