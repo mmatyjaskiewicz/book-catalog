648424202260804b02b21cd6f9e1046891f144a2
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-15 12:57:17 +0200

    feat: initialize database on startup

---
+++ Infrastructure/Persistence/DatabaseInitializer.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence;
+
+public static class DatabaseInitializer
+{
+    public static async Task InitializeAsync(BookCatalogDbContext context)
+    {
+        await context.Database.MigrateAsync();
+    }
+}
