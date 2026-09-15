f3d8946550d6b5258c275568a7c703432df85303
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-15 18:13:45 +0200

    fix: validate loan creation request

---
+++ Application/Validators/CreateLoanRequestValidator.cs
@@ -0,0 +1,18 @@
+﻿using Application.DTOs.Requests;
+using FluentValidation;
+
+namespace Application.Validators;
+
+public class CreateLoanRequestValidator : AbstractValidator<CreateLoanRequest>
+{
+    public CreateLoanRequestValidator()
+    {
+        RuleFor(x => x.BookId)
+            .NotEmpty()
+            .WithMessage("BookId is required.");
+
+        RuleFor(x => x.UserId)
+            .NotEmpty()
+            .WithMessage("UserId is required.");
+    }
+}
