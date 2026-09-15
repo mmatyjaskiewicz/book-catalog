56a4b6abeadbe3fa07f21e17e1f74a47ad06a319
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-15 13:35:50 +0200

    feat: initialize and seed database on startup

--- Infrastructure/Persistence/DatabaseInitializer.cs
+++ Infrastructure/Persistence/DatabaseInitializer.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
 
 namespace Infrastructure.Persistence;
 
@@ -7,5 +8,43 @@ public static class DatabaseInitializer
     public static async Task InitializeAsync(BookCatalogDbContext context)
     {
         await context.Database.MigrateAsync();
+        await SeedAsync(context);
+    }
+    
+    private static async Task SeedAsync(BookCatalogDbContext dbContext)
+    {
+        if (await dbContext.Authors.AnyAsync())
+        {
+            return;
+        }
+
+        var authors = new[]
+        {
+            new Author("George Orwell"),
+            new Author("J.R.R. Tolkien"),
+            new Author("Frank Herbert")
+        };
+
+        dbContext.Authors.AddRange(authors);
+
+        var users = new[]
+        {
+            new User("user"),
+            new User("user_mock"),
+            new User("user_test")
+        };
+
+        dbContext.Users.AddRange(users);
+
+        var books = new[]
+        {
+            new Book("1984", authors[0].Id, 1949),
+            new Book("The Hobbit", authors[1].Id, 1937),
+            new Book("Dune", authors[2].Id, 1965)
+        };
+
+        dbContext.Books.AddRange(books);
+
+        await dbContext.SaveChangesAsync();
     }
 }
