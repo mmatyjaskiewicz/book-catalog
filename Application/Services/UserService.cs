068027f161d36a1e6bf9c80861771cbfa8902c2c
Maksym Matyjaskiewicz <maksymalniee2@gmail.com>
2026-09-17 15:59:26 +0200

    feat: create user service for basic crud operations

---
+++ Application/Services/UserService.cs
@@ -0,0 +1,28 @@
+﻿using Application.Interfaces.Repositories;
+using Domain.Entities;
+
+namespace Application.Services;
+
+public class UserService(IUserRepository userRepository)
+{
+    public async Task CreateUserAsync(string username)
+    {
+        var user = new User(username);
+        await userRepository.AddAsync(user);
+    }
+    
+    public Task UpdateUserAsync(User user)
+    {
+        return userRepository.UpdateAsync(user);
+    }
+    
+    public Task DeleteUserAsync(User user)
+    {
+        return userRepository.DeleteAsync(user);
+    }
+    
+    public Task<User?> GetUserByIdAsync(Guid id)
+    {
+        return userRepository.GetByIdAsync(id);
+    }
+}
